a21d63bd976d0eda3ae5947a5c99cae8a21f6599
availchetan <43181143+availchetan@users.noreply.github.com>
2018-09-12 04:39:16 +0530

    Update ExtentReporter

--- my-app/src/main/java/Resources/ExtentReporterNG.java
+++ my-app/src/main/java/Resources/ExtentReporterNG.java
@@ -2,16 +2,14 @@ package Resources;
 
 import java.io.File;
 import java.util.List;
-import java.util.Map;
- 
+import java.util.Map; 
 import org.testng.IReporter;
 import org.testng.IResultMap;
 import org.testng.ISuite;
 import org.testng.ISuiteResult;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
-import org.testng.xml.XmlSuite;
- 
+import org.testng.xml.XmlSuite; 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
